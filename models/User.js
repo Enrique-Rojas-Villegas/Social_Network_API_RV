6f7467ef73fab4dc774558584c8fd1d9535dcf31
Enoc Rojas <rojiyoyo@gmail.com>
2022-04-20 19:35:36 -0500

    Added schema for userm as well as a virtual

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/], 
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+            id: false,
+        }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
